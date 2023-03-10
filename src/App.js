3162e8d5a0eee7a502b4edd6574948898f98f37c
TienCNTT <20200892@eaut.edu.vn>
2023-03-10 08:57:17 +0700

    thuc hanh 2

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { SpaceLocation } from "./typeScript/typeScript";
+
+function App() {
+
+  return (
+    <div className="App">
+      <SpaceLocation/>
+    </div>
+  );
+}
+
+export default App;
